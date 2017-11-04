48c86cf8e20c2de6e8c5645ee130102b7ba66fd6
anoyetta <anoyetta@gmail.com>
2017-11-04 12:31:14 +0900

    整備

---
+++ ACT.Hojoring/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACT.Hojoring")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACT.Hojoring")]
+[assembly: AssemblyCopyright("Copyright © 2017, anoyetta")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cc751668-0cb9-4bde-af31-ba99da079e59")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ add_submodules.bat
@@ -0,0 +1,6 @@
+git submodule add https://github.com/anoyetta/FFXIV.Framework.git FFXIV.Framework
+git submodule add https://github.com/anoyetta/ACT.TTSYukkuri.git ACT.TTSYukkuri
+git submodule add https://github.com/anoyetta/ACT.SpecialSpellTimer.git ACT.SpecialSpellTimer
+git submodule add https://github.com/anoyetta/ACT.UltraScouter.git ACT.UltraScouter
+
+pause
