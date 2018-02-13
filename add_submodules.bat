3e32604e70da43ede93ed1b585f6d655db9939b1
anoyetta <anoyetta@gmail.com>
2018-02-13 09:06:24 +0900

    FFXUV_MemoryReaderを追加した

--- add_submodules.bat
+++ add_submodules.bat
@@ -3,4 +3,6 @@ git submodule add https://github.com/anoyetta/ACT.TTSYukkuri.git ACT.TTSYukkuri
 git submodule add https://github.com/anoyetta/ACT.SpecialSpellTimer.git ACT.SpecialSpellTimer
 git submodule add https://github.com/anoyetta/ACT.UltraScouter.git ACT.UltraScouter
 
+git submodule add https://github.com/TamanegiMage/FFXIV_MemoryReader.git FFXIV_MemoryReader
+
 pause
