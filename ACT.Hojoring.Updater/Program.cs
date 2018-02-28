5960e6d46a557060c7a4361be129c64b31bb468d
anoyetta <anoyetta@gmail.com>
2018-02-28 16:20:58 +0900

    * スペスペたいむに notice="auto" 通知内容の自動生成命令を追加した * アップデート用のスクリプトを追加した

---
+++ ACT.Hojoring.Updater/Program.cs
@@ -0,0 +1,139 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace ACT.Hojoring.Updater
+{
+    public class Program
+    {
+        private const string AuthToken = "46200a9def12b2f5c765ab41f2b44e5bb4e0f0f7";
+
+        public static void Main(
+            string[] args)
+        {
+            try
+            {
+                var dest = "update";
+                var usePre = false;
+
+                if (args.Length > 0)
+                {
+                    dest = args[0];
+                    bool.TryParse(args[1], out usePre);
+                }
+
+                var client = new GitHubClient(new ProductHeaderValue("ACT.Hojoring.Updater"));
+                var token = new Credentials(AuthToken);
+                client.Credentials = token;
+
+                var releases = client.Repository.Release.GetAll("anoyetta", "ACT.Hojoring").Result;
+
+                var lastest = releases.FirstOrDefault(x => x.Prerelease == usePre);
+
+                if (lastest == null)
+                {
+                    return;
+                }
+
+                Console.WriteLine($"ver: {lastest.Name}");
+                Console.WriteLine($"tag: {lastest.Name}");
+                Console.WriteLine(string.Empty);
+
+                using (var sr = new StringReader(lastest.Body))
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+
+                    Console.WriteLine("...etc");
+                }
+
+                var asset = lastest.Assets.FirstOrDefault(x => x.Name.Contains("ACT.Hojoring"));
+                if (asset == null)
+                {
+                    return;
+                }
+
+                Console.WriteLine(string.Empty);
+
+                var file = Path.Combine(
+                    dest,
+                    asset.Name);
+
+                if (File.Exists(file))
+                {
+                    File.Delete(file);
+                }
+
+                if (Directory.Exists(dest))
+                {
+                    Directory.Delete(dest, true);
+                }
+
+                Directory.CreateDirectory(dest);
+                Thread.Sleep(200);
+
+                Task.Run(async () =>
+                {
+                    using (var web = new WebClient())
+                    {
+                        var preprogress = 0d;
+
+                        web.DownloadProgressChanged += (x, y) =>
+                        {
+                            var progress = (double)y.BytesReceived / (double)y.TotalBytesToReceive;
+                            if ((progress - preprogress) >= 0.05d)
+                            {
+                                Console.Write("+");
+                                preprogress = progress;
+                            }
+                        };
+
+                        Console.WriteLine("Downloading...");
+                        await web.DownloadFileTaskAsync(
+                            new Uri(asset.BrowserDownloadUrl),
+                            file);
+
+                        Thread.Sleep(200);
+
+                        Console.WriteLine(string.Empty);
+                        Console.WriteLine("Done!");
+                    }
+                }).Wait();
+
+#if false
+                if (!File.Exists(file))
+                {
+                    return;
+                }
+
+                Thread.Sleep(200);
+
+                var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                SevenZipBase.SetLibraryPath(Path.Combine(
+                    dir,
+                    @"tools\7z\7z.dll"));
+
+                var extractor = new SevenZipExtractor(file);
+                extractor.ExtractArchive(dest);
+
+                Console.WriteLine("Extracted!");
+
+                if (File.Exists(file))
+                {
+                    File.Delete(file);
+                }
+#endif
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ ACT.Hojoring.Updater/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACT.Hojoring.Updater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACT.Hojoring.Updater")]
+[assembly: AssemblyCopyright("Copyright © 2018, anoyetta")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("611cc237-0ac4-4ab1-9f31-6e1bf589ca14")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
