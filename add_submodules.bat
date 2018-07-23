65390597c4f38e0ca99ffd5bffa89c0e53f6a4d5
anoyetta <anoyetta@gmail.com>
2018-07-24 07:36:26 +0900

    update submodule

--- add_submodules.bat
+++ add_submodules.bat
@@ -1,8 +1,3 @@
-git submodule add https://github.com/anoyetta/FFXIV.Framework.git FFXIV.Framework
-git submodule add https://github.com/anoyetta/ACT.TTSYukkuri.git ACT.TTSYukkuri
-git submodule add https://github.com/anoyetta/ACT.SpecialSpellTimer.git ACT.SpecialSpellTimer
-git submodule add https://github.com/anoyetta/ACT.UltraScouter.git ACT.UltraScouter
-
 git submodule add https://github.com/TamanegiMage/FFXIV_MemoryReader.git FFXIV_MemoryReader
 
 pause
