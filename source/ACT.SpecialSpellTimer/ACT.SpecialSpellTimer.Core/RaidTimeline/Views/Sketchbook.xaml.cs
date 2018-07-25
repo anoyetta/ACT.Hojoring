92d2595de56cc9510f61dfe24b613620a691d3d3
anoyetta <anoyetta@gmail.com>
2018-07-25 14:16:38 +0900

    整形

--- source/ACT.SpecialSpellTimer/ACT.SpecialSpellTimer.Core/RaidTimeline/Views/Sketchbook.xaml.cs
+++ source/ACT.SpecialSpellTimer/ACT.SpecialSpellTimer.Core/RaidTimeline/Views/Sketchbook.xaml.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ACT.SpecialSpellTimer.RaidTimeline.Views
 {
     /// <summary>
     /// Sketchbook.xaml の相互作用ロジック
     /// </summary>
-    public partial class Sketchbook : Window
+    public partial class Sketchbook :
+        Window
     {
         public Sketchbook()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
