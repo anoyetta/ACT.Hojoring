70e219755538faa48f6d4009496973c6db4aa109
anoyetta <anoyetta@gmail.com>
2018-08-10 08:24:05 +0900

    タイムラインの終了時にもろもろの開放を追加した

--- source/ACT.SpecialSpellTimer/ACT.SpecialSpellTimer.Core/Properties/AssemblyInfo.cs
+++ source/ACT.SpecialSpellTimer/ACT.SpecialSpellTimer.Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a71552bd-0ca6-48b2-bdb2-66447aef9c99")]
-[assembly: AssemblyVersion("2.6.0.10")]
-[assembly: AssemblyFileVersion("2.6.0.10")]
+[assembly: AssemblyVersion("2.7.0.1")]
+[assembly: AssemblyFileVersion("2.7.0.1")]
