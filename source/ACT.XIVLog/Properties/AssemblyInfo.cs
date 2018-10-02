5bd6aae540f794ac0785b972754eaddea2144951
anoyetta <anoyetta@gmail.com>
2018-10-02 09:15:50 +0900

    XIVLogをリファクタリングした

--- source/ACT.XIVLog/Properties/AssemblyInfo.cs
+++ source/ACT.XIVLog/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d504e286-56ec-494f-82cd-fd71aefef606")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
