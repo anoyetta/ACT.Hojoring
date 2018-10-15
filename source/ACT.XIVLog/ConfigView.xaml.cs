bf1282b6dcce46f401e59b0423c854fc69330f8c
anoyetta <anoyetta@gmail.com>
2018-10-15 16:17:14 +0900

    XIVLog の改善
    * バージョン表示を追加した
    * ログフォルダを開く機能を追加した
    * 細かなバグの修正

--- source/ACT.XIVLog/ConfigView.xaml.cs
+++ source/ACT.XIVLog/ConfigView.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Advanced_Combat_Tracker;
 using Prism.Commands;
 
 namespace ACT.XIVLog
@@ -22,6 +23,23 @@ namespace ACT.XIVLog
 
         public XIVLogPlugin Plugin => XIVLogPlugin.Instance;
 
+        public string VersionInfo
+        {
+            get
+            {
+                var result = string.Empty;
+
+                var plugin = ActGlobals.oFormActMain.PluginGetSelfData(XIVLogPlugin.Instance);
+                if (plugin != null)
+                {
+                    var vi = FileVersionInfo.GetVersionInfo(plugin.pluginFile.FullName);
+                    result = $"{vi.ProductName} v{vi.FileVersion}";
+                }
+
+                return result;
+            }
+        }
+
         private ICommand oepnLogCommand;
 
         public ICommand OpenLogCommand =>
@@ -30,6 +48,19 @@ namespace ACT.XIVLog
                 if (File.Exists(this.Plugin.LogfileName))
                 {
                     Process.Start(this.Plugin.LogfileName);
+                }
+            })));
+
+        private ICommand oepnLogDirectoryCommand;
+
+        public ICommand OpenLogDirectoryCommand =>
+            this.oepnLogDirectoryCommand ?? (this.oepnLogDirectoryCommand = new DelegateCommand(async () => await Task.Run(() =>
+            {
+                var directory = Path.GetDirectoryName(this.Plugin.LogfileName);
+
+                if (Directory.Exists(directory))
+                {
+                    Process.Start(directory);
                 }
             })));
     }
