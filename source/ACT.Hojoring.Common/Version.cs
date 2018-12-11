d2345a3b7944feae44d99fe0e011769a6655ebef
anoyetta <anoyetta@gmail.com>
2018-12-11 10:16:49 +0900

    update minor

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.24.0.1")]
-[assembly: AssemblyFileVersion("5.24.0.1")]
+[assembly: AssemblyVersion("5.24.0.2")]
+[assembly: AssemblyFileVersion("5.24.0.2")]
