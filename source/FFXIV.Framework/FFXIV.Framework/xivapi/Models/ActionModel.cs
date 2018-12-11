b4fac40edbf1d912b78cded75a55d7e43769d9dd
anoyetta <anoyetta@gmail.com>
2018-12-11 15:21:17 +0900

    iconダウンローダをxiviapi版に変更した

---
+++ source/FFXIV.Framework/FFXIV.Framework/xivapi/Models/ActionModel.cs
@@ -0,0 +1,159 @@
+using FFXIV.Framework.FFXIVHelper;
+
+namespace FFXIV.Framework.xivapi.Models
+{
+    public class ActionModel
+    {
+        public readonly static string HandledColumns = $"{nameof(ID)},{nameof(Name)},{nameof(ActionCategory)}.ID,{nameof(ClassJob)}.ID,{nameof(Icon)}";
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ActionCategoryModel ActionCategory { get; set; }
+        public ClassJobModel ClassJob { get; set; }
+        public string Icon { get; set; }
+
+        public override string ToString()
+            => $"ID={this.ID} Name={this.Name}";
+    }
+
+    public class ActionCategoryModel
+    {
+        public int? ID { get; set; }
+        public string Name { get; set; }
+        public string Name_de { get; set; }
+        public string Name_en { get; set; }
+        public string Name_fr { get; set; }
+        public string Name_ja { get; set; }
+    }
+
+    public class ClassJobModel
+    {
+        public int? ID { get; set; }
+
+        public Job ToLocalJob() => this.ID.HasValue ?
+            Jobs.Find(this.ID.Value) :
+            new Job();
+
+        public ActionUserCategory ToLocalCategory()
+        {
+            switch (this.ToLocalJob().ID)
+            {
+                // タンク
+                case JobIDs.PLD:
+                case JobIDs.GLA:
+                    return ActionUserCategory.Paladin;
+
+                case JobIDs.WAR:
+                case JobIDs.MRD:
+                    return ActionUserCategory.Warrior;
+
+                case JobIDs.DRK:
+                    return ActionUserCategory.DarkKnight;
+
+                // ヒーラー
+                case JobIDs.WHM:
+                case JobIDs.CNJ:
+                    return ActionUserCategory.WhiteMage;
+
+                case JobIDs.SCH:
+                    return ActionUserCategory.Scholar;
+
+                case JobIDs.AST:
+                    return ActionUserCategory.Astrologian;
+
+                // メレー
+                case JobIDs.MNK:
+                case JobIDs.PUG:
+                    return ActionUserCategory.Monk;
+
+                case JobIDs.DRG:
+                case JobIDs.LNC:
+                    return ActionUserCategory.Dragoon;
+
+                case JobIDs.NIN:
+                case JobIDs.ROG:
+                    return ActionUserCategory.Ninja;
+
+                case JobIDs.SAM:
+                    return ActionUserCategory.Samurai;
+
+                // レンジ
+                case JobIDs.BRD:
+                case JobIDs.ARC:
+                    return ActionUserCategory.Bard;
+
+                case JobIDs.MCH:
+                    return ActionUserCategory.Machinist;
+
+                // マジック
+                case JobIDs.BLM:
+                case JobIDs.THM:
+                    return ActionUserCategory.BlackMage;
+
+                case JobIDs.SMN:
+                case JobIDs.ACN:
+                    return ActionUserCategory.Summoner;
+
+                case JobIDs.RDM:
+                    return ActionUserCategory.RedMage;
+            }
+
+            return ActionUserCategory.Nothing;
+        }
+    }
+
+    public enum ActionCategory
+    {
+        AutoAttack = 1,
+        Spell,
+        Weaponskill,
+        Ability,
+        Item,
+        DoLAbility,
+        DoHAbility,
+        Event,
+        System,
+        Artillery,
+        Mount,
+        Glamour,
+        AdrenalineRush
+    }
+
+    /// <summary>
+    /// Hojoringとしてアクションの分類するためのカテゴリ分け
+    /// </summary>
+    public enum ActionUserCategory
+    {
+        Nothing = 0,
+
+        TankRole = 10,
+        Paladin = 11,
+        Warrior = 12,
+        DarkKnight = 13,
+
+        HealerRole = 20,
+        WhiteMage = 21,
+        Scholar = 22,
+        Astrologian = 23,
+
+        MeleeDPSRole = 30,
+        Monk = 31,
+        Dragoon = 32,
+        Ninja = 33,
+        Samurai = 34,
+
+        RangeDPSRole = 40,
+        Bard = 41,
+        Machinist = 42,
+
+        MagicDPSRole = 50,
+        BlackMage = 51,
+        Summoner = 52,
+        RedMage = 53,
+
+        PetsEgi = 61,
+        PetsFairy = 62,
+
+        Others = 90,
+    }
+}
---
+++ source/FFXIV.Framework/FFXIV.Framework/xivapi/XIVAPIController.cs
@@ -0,0 +1,232 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using FFXIV.Framework.FFXIVHelper;
+using FFXIV.Framework.Globalization;
+using FFXIV.Framework.xivapi.Models;
+using Newtonsoft.Json;
+
+namespace FFXIV.Framework.xivapi
+{
+    public class XIVAPIController
+    {
+        #region Singleton
+
+        private static XIVAPIController instance;
+
+        public static XIVAPIController Instance => instance ?? (instance = new XIVAPIController());
+
+        private XIVAPIController()
+        {
+            this.InitializeClient();
+        }
+
+        #endregion Singleton
+
+        public static readonly Uri BaseAddress = new Uri("https://xivapi.com/");
+
+        private readonly HttpClient client = new HttpClient();
+
+#if DEBUG
+        private static readonly string ApiKey = "b1a9c1f77b9a434e8ac05f04";
+#else
+        private static readonly string ApiKey = null;
+#endif
+        private const int DefaultLimitCount = 3000;
+
+        private void InitializeClient()
+        {
+            this.client.BaseAddress = BaseAddress;
+            this.client.DefaultRequestHeaders.Accept.Clear();
+            this.client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public Locales Language { get; set; } = Locales.EN;
+
+        public NameValueCollection CreateQueryStringParser()
+            => HttpUtility.ParseQueryString(string.Empty);
+
+        public async Task<HttpResponseMessage> GetAsync(
+            string uri,
+            NameValueCollection queryString = null)
+        {
+            if (queryString != null)
+            {
+                uri += "?" + queryString.ToString();
+            }
+
+            return await this.client.GetAsync(uri);
+        }
+
+        public delegate void ProgressDownloadCallbak(DownloadProgressEventArgs args);
+
+        #region xivapi.com/Action
+
+        public async Task<IEnumerable<ActionModel>> GetActionsAsync(
+            ProgressDownloadCallbak callback = null)
+        {
+            const string MethodUri = "Action";
+
+            var result = default(ApiResultModel<List<ActionModel>>);
+            var actionList = new List<ActionModel>();
+            var currentPage = 1;
+
+            do
+            {
+                var query = this.CreateQueryStringParser();
+                query["language"] = this.Language.ToString().ToLower();
+                query["limit"] = DefaultLimitCount.ToString();
+                query["page"] = currentPage.ToString();
+                query["columns"] = ActionModel.HandledColumns;
+#if DEBUG
+                query["pretty"] = "1";
+#endif
+                if (!string.IsNullOrEmpty(ApiKey))
+                {
+                    query["key"] = ApiKey;
+                }
+
+                var res = await this.GetAsync(MethodUri, query);
+                var json = await res?.Content?.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(json))
+                {
+                    result = JsonConvert.DeserializeObject<ApiResultModel<List<ActionModel>>>(json);
+                }
+
+                if (result == null ||
+                    result.Pagination == null)
+                {
+                    break;
+                }
+
+                actionList.AddRange(
+                    from x in result.Results
+                    where
+                    x.ActionCategory.ID.HasValue &&
+                    x.ClassJob.ID.HasValue &&
+                    x.ClassJob.ID != (int)JobIDs.ADV &&
+                    x.ActionCategory.ID != (int)ActionCategory.AutoAttack &&
+                    x.ActionCategory.ID != (int)ActionCategory.Item &&
+                    x.ActionCategory.ID != (int)ActionCategory.Event &&
+                    x.ActionCategory.ID != (int)ActionCategory.System &&
+                    x.ActionCategory.ID != (int)ActionCategory.Mount &&
+                    x.ActionCategory.ID != (int)ActionCategory.Glamour &&
+                    x.ActionCategory.ID != (int)ActionCategory.AdrenalineRush &&
+                    !string.IsNullOrEmpty(x.Name) &&
+                    x.ClassJob.ToLocalCategory() != ActionUserCategory.Nothing
+                    select
+                    x);
+
+                if (callback != null)
+                {
+                    callback.Invoke(
+                        new DownloadProgressEventArgs()
+                        {
+                            Current = currentPage,
+                            Max = result.Pagination.PageTotal,
+                            CurrentObject = result.Results
+                        });
+                }
+
+                currentPage++;
+                await Task.Delay(TimeSpan.FromSeconds(1.05));
+            } while (result.Pagination.Page < result.Pagination.PageTotal);
+
+            return
+                from x in actionList
+                orderby
+                x.ClassJob.ToLocalCategory(),
+                x.ID
+                select
+                x;
+        }
+
+        public async Task DownloadActionIcons(
+            string saveDirectory,
+            IEnumerable<ActionModel> actions,
+            ProgressDownloadCallbak callback = null)
+        {
+            // ファイル名に使えない文字を取得しておく
+            var invalidChars = Path.GetInvalidFileNameChars();
+
+            var iconBaseDirectory = Path.Combine(
+                saveDirectory,
+                "Action icons");
+
+            var actionsByCategory =
+                from x in actions
+                orderby
+                x.ClassJob.ToLocalCategory(),
+                x.ID
+                group x by
+                x.ClassJob.ToLocalCategory();
+
+            var current = 1;
+            using (var wc = new WebClient())
+            {
+                foreach (var category in actionsByCategory)
+                {
+                    var key = category.Key;
+
+                    var dirName =
+                        $"{((int)key).ToString("00")}_{Enum.GetName(typeof(ActionUserCategory), key)}";
+
+                    var dir = Path.Combine(iconBaseDirectory, dirName);
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+
+                    foreach (var skill in category)
+                    {
+                        var fileName =
+                            $"{skill.ID.ToString("0000")}_{skill.Name}.png";
+
+                        // ファイル名に使えない文字を除去する
+                        fileName = string.Concat(fileName.Where(c =>
+                            !invalidChars.Contains(c)));
+
+                        var file = Path.Combine(dir, fileName);
+                        if (File.Exists(file))
+                        {
+                            File.Delete(file);
+                        }
+
+                        var uri = BaseAddress.ToString() + "/" + skill.Icon;
+                        await wc.DownloadFileTaskAsync(uri, file);
+
+                        if (callback != null)
+                        {
+                            callback.Invoke(new DownloadProgressEventArgs()
+                            {
+                                Current = current,
+                                Max = actions.Count(),
+                                CurrentObject = Path.Combine(dirName, fileName),
+                            });
+                        }
+
+                        current++;
+                        await Task.Delay(TimeSpan.FromSeconds(0.05));
+                    }
+                }
+            }
+        }
+
+        #endregion xivapi.com/Action
+    }
+
+    public class DownloadProgressEventArgs : EventArgs
+    {
+        public int Current { get; set; }
+        public int Max { get; set; }
+        public object CurrentObject { get; set; }
+    }
+}
