8ddb2de015cae8463468f0be176c127eb3d8aad3
anoyetta <anoyetta@gmail.com>
2019-01-11 08:17:09 +0900

    統計データベースのダウンロード処理を厳密化した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.28.0.2")]
-[assembly: AssemblyFileVersion("5.28.0.2")]
+[assembly: AssemblyVersion("5.28.0.3")]
+[assembly: AssemblyFileVersion("5.28.0.3")]
