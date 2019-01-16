6a44df2000dce3c126cab6e9a955c0b14d4c3b42
anoyetta <anoyetta@gmail.com>
2019-01-16 14:37:24 +0900

    updated #148
    サポート情報のバックアップ対象にtimelineディレクトリを加えた

---
+++ source/FFXIV.Framework/FFXIV.Framework/Bridge/HelpBridge.cs
@@ -0,0 +1,23 @@
+namespace FFXIV.Framework.Bridge
+{
+    public class HelpBridge
+    {
+        #region Singleton
+
+        private static HelpBridge instance;
+
+        public static HelpBridge Instance =>
+            instance ?? (instance = new HelpBridge());
+
+        private HelpBridge()
+        {
+        }
+
+        #endregion Singleton
+
+        public delegate void BackupDelegate(
+            string destinationDirectory);
+
+        public BackupDelegate BackupCallback;
+    }
+}
