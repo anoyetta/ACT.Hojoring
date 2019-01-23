b74cf18ed54679ab070fcde8e3682d72e75c8f93
anoyetta <anoyetta@gmail.com>
2019-01-23 11:28:28 +0900

    Enmityの構造を変更した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("5.30.0.3")]
-[assembly: AssemblyFileVersion("5.30.0.3")]
+[assembly: AssemblyVersion("5.30.0.4")]
+[assembly: AssemblyFileVersion("5.30.0.4")]
