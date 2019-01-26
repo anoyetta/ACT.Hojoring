f23851e42de71da042ddb50975afcfa77576a800
anoyetta <anoyetta@gmail.com>
2019-01-26 13:27:42 +0900

    アクションバーの描画方法を調整した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
-[assembly: AssemblyVersion("5.30.0.8")]
-[assembly: AssemblyFileVersion("5.30.0.8")]
+[assembly: AssemblyVersion("5.30.0.9")]
+[assembly: AssemblyFileVersion("5.30.0.9")]
