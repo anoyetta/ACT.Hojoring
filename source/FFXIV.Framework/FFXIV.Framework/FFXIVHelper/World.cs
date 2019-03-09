d0ad6c61548f40d3a4dab23b9dab69af206a60a9
anoyetta <anoyetta@gmail.com>
2019-03-09 11:49:28 +0900

    release v6.1.2-190309.1149

--- source/FFXIV.Framework/FFXIV.Framework/FFXIVHelper/World.cs
+++ source/FFXIV.Framework/FFXIV.Framework/FFXIVHelper/World.cs
@@ -2,7 +2,7 @@ namespace FFXIV.Framework.FFXIVHelper
 {
     public class World
     {
-        public int ID { get; set; }
+        public uint ID { get; set; }
 
         public string Name { get; set; }
     }
