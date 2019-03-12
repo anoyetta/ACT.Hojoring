2f5f29c79164efcfa43066fd615ce3e192ae9788
anoyetta <anoyetta@gmail.com>
2019-03-12 16:07:55 +0900

    version up

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("6.2.0.0")]
-[assembly: AssemblyFileVersion("6.2.0.0")]
+[assembly: AssemblyVersion("6.2.0.1")]
+[assembly: AssemblyFileVersion("6.2.0.1")]
