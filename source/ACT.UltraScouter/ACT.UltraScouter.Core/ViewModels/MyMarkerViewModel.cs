b372d5ca48aac7055747e194c272e559001e952d
anoyetta <anoyetta@gmail.com>
2019-05-21 10:26:34 +0900

    MyMarkerに非戦闘時は非表示にするオプションを追加した

--- source/ACT.UltraScouter/ACT.UltraScouter.Core/ViewModels/MyMarkerViewModel.cs
+++ source/ACT.UltraScouter/ACT.UltraScouter.Core/ViewModels/MyMarkerViewModel.cs
@@ -1,5 +1,6 @@
 using ACT.UltraScouter.Config;
 using ACT.UltraScouter.ViewModels.Bases;
+using FFXIV.Framework.FFXIVHelper;
 
 namespace ACT.UltraScouter.ViewModels
 {
@@ -25,6 +26,23 @@ namespace ACT.UltraScouter.ViewModels
 
         public virtual MyMarker Config => Settings.Instance.MyMarker;
 
-        public bool OverlayVisible => this.Config.Visible;
+        public bool OverlayVisible
+        {
+            get
+            {
+                if (!this.Config.Visible)
+                {
+                    return false;
+                }
+
+                if (this.Config.HideInNotCombat &&
+                    !FFXIVPlugin.Instance.InCombat)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
     }
 }
