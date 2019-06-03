b1487782b6b8bd738c4023338877ca02a3373251
anoyetta <anoyetta@gmail.com>
2019-06-03 18:19:30 +0900

    release v6.9.10-190603.1819

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("6.9.0.9")]
-[assembly: AssemblyFileVersion("6.9.0.9")]
+[assembly: AssemblyVersion("6.9.0.10")]
+[assembly: AssemblyFileVersion("6.9.0.10")]
