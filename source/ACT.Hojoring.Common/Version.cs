a990c7ae7eac440139905b7ba03ccba1b6fafd77
anoyetta <anoyetta@gmail.com>
2019-06-25 15:32:15 +0900

    v7-alpha13

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.0.0.12")]
-[assembly: AssemblyFileVersion("7.0.0.12")]
+[assembly: AssemblyVersion("7.0.0.13")]
+[assembly: AssemblyFileVersion("7.0.0.13")]
