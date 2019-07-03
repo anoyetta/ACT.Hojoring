bd1e916c37d53d5789f635302cb862dd1a7878c3
anoyetta <anoyetta@gmail.com>
2019-07-03 17:33:30 +0900

    モブリストを更新した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.1.0.8")]
-[assembly: AssemblyFileVersion("7.1.0.8")]
+[assembly: AssemblyVersion("7.1.0.9")]
+[assembly: AssemblyFileVersion("7.1.0.9")]
