a8d9fffa45371b599ef7328f9fccc37282f929bb
anoyetta <anoyetta@gmail.com>
2019-08-21 11:16:32 +0900

    fixed bug

--- source/ACT.SpecialSpellTimer/ACT.SpecialSpellTimer.Core/PluginMainWorker.cs
+++ source/ACT.SpecialSpellTimer/ACT.SpecialSpellTimer.Core/PluginMainWorker.cs
@@ -407,7 +407,8 @@ namespace ACT.SpecialSpellTimer
             var spells = TableCompiler.Instance.SpellList;
             foreach (var spell in spells)
             {
-                exists |= SpellsController.Instance.UpdateHotbarRecast(spell);
+                exists |= spell.UseHotbarRecastTime && !string.IsNullOrEmpty(spell.HotbarName);
+                SpellsController.Instance.UpdateHotbarRecast(spell);
                 Thread.Yield();
             }
 
