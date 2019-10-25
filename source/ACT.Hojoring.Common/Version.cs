464bb687d04a602c16b89bbb8b43df48140fd9aa
anoyetta <anoyetta@gmail.com>
2019-10-25 15:49:59 +0900

    Enmityワーカーの終了時に例外が発生する場合がある不具合を修正した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.8.0.10")]
-[assembly: AssemblyFileVersion("7.8.0.10")]
+[assembly: AssemblyVersion("7.8.0.11")]
+[assembly: AssemblyFileVersion("7.8.0.11")]
