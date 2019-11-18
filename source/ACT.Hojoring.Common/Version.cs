174bb5b9e716054ce49c49b061d3f8a6c17008b2
anoyetta <anoyetta@gmail.com>
2019-11-18 20:11:07 +0900

    release v7.9.4-191118.2011

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.9.0.3")]
-[assembly: AssemblyFileVersion("7.9.0.3")]
+[assembly: AssemblyVersion("7.9.0.4")]
+[assembly: AssemblyFileVersion("7.9.0.4")]
