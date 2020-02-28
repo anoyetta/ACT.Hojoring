a01024996abc8a0823344f4fc5aad49d8cbcee9b
anoyetta <anoyetta@gmail.com>
2020-02-28 10:44:37 +0900

    release v7.14.4-200228.1044

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.14.0.3")]
-[assembly: AssemblyFileVersion("7.14.0.3")]
+[assembly: AssemblyVersion("7.14.0.4")]
+[assembly: AssemblyFileVersion("7.14.0.4")]
