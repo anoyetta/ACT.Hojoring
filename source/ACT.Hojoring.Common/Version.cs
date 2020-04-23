e6371cb6729ab6f683dde45ee14da74a04afd6ce
anoyetta <anoyetta@gmail.com>
2020-04-23 16:04:42 +0900

    テーブルの排他制御を修正した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.15.0.9")]
-[assembly: AssemblyFileVersion("7.15.0.9")]
+[assembly: AssemblyVersion("7.15.0.10")]
+[assembly: AssemblyFileVersion("7.15.0.10")]
