2998537ac6126fbf1540d74b726acb2756999e68
anoyetta <anoyetta@gmail.com>
2020-11-26 09:04:33 +0900

    コンテンツからの退出などにおいてパーティメンバーの変更を連続して検知してしまう問題に対策を実施した

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("7.23.0.3")]
-[assembly: AssemblyFileVersion("7.23.0.3")]
+[assembly: AssemblyVersion("7.23.0.4")]
+[assembly: AssemblyFileVersion("7.23.0.4")]
