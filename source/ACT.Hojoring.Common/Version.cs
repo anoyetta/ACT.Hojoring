8d6bec29d673947e6fb6468c6c5ff2101efbf441
anoyetta <anoyetta@gmail.com>
2020-12-18 00:41:33 +0900

    release v8.0.7-201218.0041

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("8.0.0.6")]
-[assembly: AssemblyFileVersion("8.0.0.6")]
+[assembly: AssemblyVersion("8.0.0.7")]
+[assembly: AssemblyFileVersion("8.0.0.7")]
