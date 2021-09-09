f72327e413c4f4bb4fe7688524049afc48a11f43
anoyetta <anoyetta@gmail.com>
2021-09-10 00:35:37 +0900

    AWSPollyのVoiceの種類を更新できるようにした

---
+++ source/ACT.Hojoring.Debug/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACT.Hojoring.Debug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACT.Hojoring.Debug")]
+[assembly: AssemblyCopyright("Copyright © anoyetta 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("466c29aa-d69a-4d20-aa75-f559d9528634")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- source/ACT.TTSYukkuri/ACT.TTSYukkuri.Core/Config/ViewModels/PollyConfigViewModel.cs
+++ source/ACT.TTSYukkuri/ACT.TTSYukkuri.Core/Config/ViewModels/PollyConfigViewModel.cs
@@ -1,18 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using ACT.TTSYukkuri.SAPI5;
 using Amazon;
 using Amazon.Polly;
-using Prism.Mvvm;
+using Amazon.Polly.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
 using FFXIV.Framework.Bridge;
+using FFXIV.Framework.WPF.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
 
 namespace ACT.TTSYukkuri.Config.ViewModels
 {
-
     public class PollyConfigViewModel : BindableBase
     {
-        VoicePalettes VoicePalette { get; set; }
+        private VoicePalettes VoicePalette { get; set; }
 
         public PollyConfigViewModel(VoicePalettes voicePalette = VoicePalettes.Default)
         {
@@ -29,15 +35,19 @@ namespace ACT.TTSYukkuri.Config.ViewModels
                     case VoicePalettes.Default:
                         config = Settings.Default.PollySettings;
                         break;
+
                     case VoicePalettes.Ext1:
                         config = Settings.Default.PollySettingsExt1;
                         break;
+
                     case VoicePalettes.Ext2:
                         config = Settings.Default.PollySettingsExt2;
                         break;
+
                     case VoicePalettes.Ext3:
                         config = Settings.Default.PollySettingsExt3;
                         break;
+
                     default:
                         config = Settings.Default.PollySettings;
                         break;
@@ -54,29 +64,7 @@ namespace ACT.TTSYukkuri.Config.ViewModels
                 Value = x.SystemName
             });
 
-        public IEnumerable<dynamic> Voices => new[]
-        {
-            new { Name = $"{VoiceId.Ivy.Value} (en-US, Female)", Value = VoiceId.Ivy.Value },
-            new { Name = $"{VoiceId.Joanna.Value} (en-US, Female)", Value = VoiceId.Joanna.Value },
-            new { Name = $"{VoiceId.Joey.Value} (en-US, Male)", Value = VoiceId.Joey.Value },
-            new { Name = $"{VoiceId.Justin.Value} (en-US, Male)", Value = VoiceId.Justin.Value },
-            new { Name = $"{VoiceId.Kendra.Value} (en-US, Female)", Value = VoiceId.Kendra.Value },
-            new { Name = $"{VoiceId.Kimberly.Value} (en-US, Female)", Value = VoiceId.Kimberly.Value },
-            new { Name = $"{VoiceId.Matthew.Value} (en-US, Male)", Value = VoiceId.Matthew.Value },
-            new { Name = $"{VoiceId.Salli.Value} (en-US, Female)", Value = VoiceId.Salli.Value },
-
-            new { Name = $"{VoiceId.Celine.Value} (fr-FR, Female)", Value = VoiceId.Celine.Value },
-            new { Name = $"{VoiceId.Mathieu.Value} (fr-FR, Male)", Value = VoiceId.Mathieu.Value },
-
-            new { Name = $"{VoiceId.Hans.Value} (de-DE, Male)", Value = VoiceId.Hans.Value },
-            new { Name = $"{VoiceId.Marlene.Value} (de-DE, Female)", Value = VoiceId.Marlene.Value },
-            new { Name = $"{VoiceId.Vicki.Value} (de-DE, Female)", Value = VoiceId.Vicki.Value },
-
-            new { Name = $"{VoiceId.Mizuki.Value} (ja-JP, Female)", Value = VoiceId.Mizuki.Value },
-            new { Name = $"{VoiceId.Takumi.Value} (ja-JP, Male)", Value = VoiceId.Takumi.Value },
-
-            new { Name = $"{VoiceId.Seoyeon.Value} (ko-KR, Female)", Value = VoiceId.Seoyeon.Value },
-        };
+        public ObservableCollection<PollyConfigs.PollyVoice> Voices => Settings.Default.PollyVoices;
 
         public IEnumerable<dynamic> Volumes =>
             Enum.GetValues(typeof(Volumes))
@@ -104,5 +92,97 @@ namespace ACT.TTSYukkuri.Config.ViewModels
                 Name = x.ToXML(),
                 Value = x,
             });
+
+        private DelegateCommand _getVoicesCommand;
+
+        public DelegateCommand GetVoicesCommand =>
+            this._getVoicesCommand ?? (this._getVoicesCommand = new DelegateCommand(this.ExecuteGetVoicesCommand));
+
+        private async void ExecuteGetVoicesCommand()
+        {
+            if (string.IsNullOrEmpty(this.Config.AccessKey) ||
+                string.IsNullOrEmpty(this.Config.SecretKey))
+            {
+                return;
+            }
+
+            var endpoint = this.Config.Endpoint;
+            var chain = new CredentialProfileStoreChain();
+
+            var hash = (this.Config.Region + this.Config.AccessKey + this.Config.SecretKey).GetHashCode().ToString("X4");
+            var profileName = $"polly_profile_{hash}";
+
+            AWSCredentials awsCredentials;
+            if (!chain.TryGetAWSCredentials(
+                profileName,
+                out awsCredentials))
+            {
+                var options = new CredentialProfileOptions
+                {
+                    AccessKey = this.Config.AccessKey,
+                    SecretKey = this.Config.SecretKey,
+                };
+
+                var profile = new CredentialProfile(profileName, options);
+                profile.Region = endpoint;
+
+                chain.RegisterProfile(profile);
+
+                chain.TryGetAWSCredentials(
+                    profileName,
+                    out awsCredentials);
+            }
+
+            if (awsCredentials == null)
+            {
+                return;
+            }
+
+            var voice = this.Config.Voice;
+
+            using (var pc = new AmazonPollyClient(
+                awsCredentials,
+                endpoint))
+            {
+                var res = await pc.DescribeVoicesAsync(new DescribeVoicesRequest());
+
+                if (res == null ||
+                    res.HttpStatusCode != HttpStatusCode.OK)
+                {
+                    ModernMessageBox.ShowDialog(
+                        "Voices update is failed.",
+                        "ACT.Hojoring");
+
+                    return;
+                }
+
+                this.Voices.Clear();
+                this.Config.Voice = string.Empty;
+
+                foreach (var v in
+                    from x in res.Voices
+                    orderby
+                    x.LanguageCode.ToString(),
+                    x.Gender.ToString(),
+                    x.Name
+                    select
+                    x)
+                {
+                    this.Voices.Add(
+                        new PollyConfigs.PollyVoice { Name = $"{v.Id.Value} ({v.LanguageCode}, {v.Gender})", Value = v.Id });
+                }
+            }
+
+            if (this.Voices.Any(x => x.Value == voice))
+            {
+                this.Config.Voice = voice;
+            }
+
+            Settings.Default.Save();
+
+            ModernMessageBox.ShowDialog(
+                "Voices update is completed.",
+                "ACT.Hojoring");
+        }
     }
 }
