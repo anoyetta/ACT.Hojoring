4a0f8ebfeed07c56eb1cb1c957b6a3cb764a8003
anoyetta <anoyetta@gmail.com>
2021-09-15 22:44:35 +0900

    vesion up, v9.4.0

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("9.3.0.9")]
-[assembly: AssemblyFileVersion("9.3.0.9")]
+[assembly: AssemblyVersion("9.4.0.0")]
+[assembly: AssemblyFileVersion("9.4.0.0")]
