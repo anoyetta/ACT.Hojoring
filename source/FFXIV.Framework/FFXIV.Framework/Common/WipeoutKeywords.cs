70ce3c2ec44b1644a8595bd518ebef49c25c4628
anoyetta <anoyetta@gmail.com>
2021-11-06 17:50:05 +0900

    wipeoutログの判定書式を変更した

--- source/FFXIV.Framework/FFXIV.Framework/Common/WipeoutKeywords.cs
+++ source/FFXIV.Framework/FFXIV.Framework/Common/WipeoutKeywords.cs
@@ -3,7 +3,7 @@ namespace FFXIV.Framework.Common
     public static class WipeoutKeywords
     {
         public static readonly string Wipeout = "wipeout";
-        public static readonly string WipeoutLog = $"Debug FB:Hojoring:{Wipeout}";
-        public static readonly string WipeoutLogEcho = $"00:0038::{Wipeout}";
+        public static readonly string WipeoutLog = $":{Wipeout}";
+        public static readonly string WipeoutLogEcho = $":{Wipeout}";
     }
 }
