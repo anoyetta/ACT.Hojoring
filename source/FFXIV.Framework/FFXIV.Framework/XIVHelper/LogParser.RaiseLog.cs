e16b602377d77d0b85d7ed90dcb7fffe6f695e25
anoyetta <anoyetta@gmail.com>
2021-11-08 18:18:24 +0900

    RaiseLogの方式を変更した

---
+++ source/FFXIV.Framework/FFXIV.Framework/XIVHelper/LogParser.RaiseLog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Advanced_Combat_Tracker;
+using FFXIV_ACT_Plugin.Logfile;
+
+namespace FFXIV.Framework.XIVHelper
+{
+    public static partial class LogParser
+    {
+        public static void RaiseLog(
+            DateTime timestamp,
+            IEnumerable<string> logs)
+        {
+            if (!ActGlobals.oFormActMain.CanFocus)
+            {
+                return;
+            }
+
+            var output = XIVPluginHelper.Instance.LogOutput;
+            if (output == null)
+            {
+                return;
+            }
+
+            foreach (var log in logs)
+            {
+                output.WriteLine(LogMessageType.ChatLog, timestamp, FormatLogLine(log));
+            }
+        }
+
+        public static void RaiseLog(
+            DateTime timestamp,
+            string log)
+        {
+            if (!ActGlobals.oFormActMain.CanFocus)
+            {
+                return;
+            }
+
+            var output = XIVPluginHelper.Instance.LogOutput;
+            if (output == null)
+            {
+                return;
+            }
+
+            output.WriteLine(LogMessageType.ChatLog, timestamp, FormatLogLine(log));
+        }
+
+        private const string GameEchoChatCode = "0038";
+
+        private static string FormatLogLine(
+            string log)
+            => $"{GameEchoChatCode}||Hojoring>{log}";
+    }
+}
