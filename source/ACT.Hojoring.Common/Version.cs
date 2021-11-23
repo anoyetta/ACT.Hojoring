160e7119b184970723c63cbed423a8eefaa2e17c
anoyetta <anoyetta@gmail.com>
2021-11-23 15:27:22 +0900

    バージョンアップ

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("9.6.0.13")]
-[assembly: AssemblyFileVersion("9.6.0.13")]
+[assembly: AssemblyVersion("9.6.0.14")]
+[assembly: AssemblyFileVersion("9.6.0.14")]
