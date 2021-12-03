5eec85f7c7465f77c33e00cf042c05b4780464b7
anoyetta <anoyetta@gmail.com>
2021-12-03 13:27:54 +0900

    Hojoring X for EW

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("9.6.0.22")]
-[assembly: AssemblyFileVersion("9.6.0.22")]
+[assembly: AssemblyVersion("10.0.0.0")]
+[assembly: AssemblyFileVersion("10.0.0.0")]
