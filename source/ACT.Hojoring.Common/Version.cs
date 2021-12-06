60023f575f0a0bcdc18f463becdcba434a15b33f
anoyetta <anoyetta@gmail.com>
2021-12-06 17:42:58 +0900

    release v10.0.3-211206.1742

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.0.0.2")]
-[assembly: AssemblyFileVersion("10.0.0.2")]
+[assembly: AssemblyVersion("10.0.0.3")]
+[assembly: AssemblyFileVersion("10.0.0.3")]
