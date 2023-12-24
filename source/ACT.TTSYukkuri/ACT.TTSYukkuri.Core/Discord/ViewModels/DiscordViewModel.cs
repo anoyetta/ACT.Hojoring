5253e8d0bbbf2faf6283cfa4fa07d5e473d15bdd
bonbon <yosiox@gmail.com>
2023-12-25 00:23:45 +0900

    Update2
    Discord.Netに依存するNuGetパッケージの更新を最小限にした

--- source/ACT.TTSYukkuri/ACT.TTSYukkuri.Core/Discord/ViewModels/DiscordViewModel.cs
+++ source/ACT.TTSYukkuri/ACT.TTSYukkuri.Core/Discord/ViewModels/DiscordViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms.Integration;
 using System.Windows.Input;
 using ACT.TTSYukkuri.Config;
@@ -26,7 +27,18 @@ namespace ACT.TTSYukkuri.Discord.ViewModels
         public ICommand ConnectCommand =>
             this.connectCommand ?? (this.connectCommand = new DelegateCommand(async () =>
             {
-                this.Model.Connect();
+                Action action = () =>
+                {
+                    this.Model.Connect();
+                };
+                try
+                {
+                    action();
+                } catch (TypeLoadException e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+ //               this.Model.Connect();
                 await Task.Delay(TimeSpan.FromMilliseconds(100));
             }));
 
