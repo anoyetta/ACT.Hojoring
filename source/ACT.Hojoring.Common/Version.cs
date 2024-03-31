9abf87285d6f7b2313a22c4fbd8561500acdd473
bonbon <yosiox@gmail.com>
2024-04-01 01:58:54 +0900

    update
    reset-on-wipeoutに応じてXIVLogが録画を止めないようにした

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.2.0.8")]
-[assembly: AssemblyFileVersion("10.2.0.8")]
+[assembly: AssemblyVersion("10.2.0.9")]
+[assembly: AssemblyFileVersion("10.2.0.9")]
