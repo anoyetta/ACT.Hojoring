4758d7de350db6ee1a76e799684f5d2d184af69c
bonbon <yosiox@gmail.com>
2024-07-28 12:03:00 +0900

    version up

--- source/ACT.Hojoring.Common/Version.cs
+++ source/ACT.Hojoring.Common/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.3.0.4")]
-[assembly: AssemblyFileVersion("10.3.0.4")]
+[assembly: AssemblyVersion("10.3.0.5")]
+[assembly: AssemblyFileVersion("10.3.0.5")]
